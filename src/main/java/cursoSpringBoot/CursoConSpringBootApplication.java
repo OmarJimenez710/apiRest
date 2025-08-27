6f6a0f8591845b0389f98cf132d2aefbf29ac159
omarJimenezFlores <omar.jimenez@hixsa.com>
2025-08-26 19:52:33 -0600

    Creación de API básica con arquitectura REST y metodología en capas, utilizando Java 17 y spring boot como principales herrameintas

--- src/main/java/cursoSpringBoot/CursoConSpringBootApplication.java
+++ src/main/java/cursoSpringBoot/CursoConSpringBootApplication.java
@@ -9,5 +9,4 @@ public class CursoConSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CursoConSpringBootApplication.class, args);
 	}
-
 }
--- src/main/java/cursoSpringBoot/controllers/CustomerController.java
+++ src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -79,5 +79,4 @@ public class CustomerController {
 
         return customerList;
     }
-
 }
